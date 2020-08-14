eef13dc9390b389682e44d2021eb4b851a732383
Dallas James <dallasjames42@gmail.com>
2020-08-14 17:49:15 -0600

    MVP

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,10 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -16,19 +21,27 @@
 # should not be loaded into a City object.
 cities = []
 
+import csv
+
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # Ensure that the lat and lon valuse are all floats
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # Ensure that the lat and lon values are all floats
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    with open('cities.csv', newline='') as file:
+        spamreader = csv.DictReader(file)
+        for city in spamreader:
+            cities.append(City(city['city'], float(city['lat']), float(city['lng'])))
     return cities
+
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(c.name, c.lat, c.lon)
+
 
 # STRETCH GOAL!
 #
@@ -62,10 +75,10 @@ for c in cities:
 # TODO Get latitude and longitude values from the user
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
-  
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    # within will hold the cities that fall within the specified region
+    within = []
 
-  return within
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
+
+    return within
